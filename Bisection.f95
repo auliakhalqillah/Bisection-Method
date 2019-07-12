b50a1698eb8090c5a9ea8b2bf339be812b4576bd
auliakhalqillah <52442929+auliakhalqillah@users.noreply.github.com>
2019-07-12 22:32:44 +0700

    Update Bisection.f95

--- Bisection.f95
+++ Bisection.f95
@@ -7,72 +7,61 @@
 !
 ! email: auliakhalqillah.mail@gmail.com
 ! ==============================================================================
+program bisection_method
+  implicit none
+  real :: xi,xf,xr,er,xrold,f,inf
+  integer :: iter,n,it
+  character(len=100) :: fmt
 
-program bisection
-implicit none
-real,parameter::error=1e-4
-real::a,b,f,c, er, h, oldc
-integer :: iter,n,it
+  write(*,*)""
+  write(*,*)"---------------------------------"
+  write(*,*)"BISECTION METHOD - FINDING ROOT"
+  write(*,*)"---------------------------------"
+  write(*,*) ""
+  write(*,"(a)",advance="no") "Isert Initial Boundary (XI):"
+  read*, xi
+  write(*,"(a)",advance="no") "Insert Final Boundary (XF):"
+  read*, xf
+  write(*,"(a)",advance="no") "Insert Data Length (ex: N=100):"
+  read*, n
 
-write(*,*)""
-write(*,*)"=========================================================================="
-write(*,*)"                     BISECTION PROGRAM (FINDING ROOT)             "
-write(*,*)"=========================================================================="
-write(*,*)""
-write(*,"(a)",advance="no") "INSERT INITIAL BOUNDARY:"
-read*, a
-write(*,"(a)",advance="no") "INSERT FINAL BOUNDARY:"
-read*, b
-write(*,"(a)",advance="no") "INSERT STEP DATA POINT (EX: 0.1):"
-read*, h
+  inf = huge(1000.0)
+  fmt = "(a12,a13,a13,a20)"
+  write(*,*)""
+  write(*,fmt)"ITER","XR[ROOT]","F(XR)","ERROR(%)"
+  open(20, file='bisection.txt', status='replace')
 
-n = int((b - a)/h)
+  50 if ((f(xi)*f(xf)) .le. 0) then
+    iter = 1
+    xrold = 0.0
+    do while (iter .le. n)
+      xr = (xi+xf)/2
 
-open(20, file='bisection.txt')
-it = 1
-500 if (f(a)*f(b) <= 0) then
-iter = 1
+      if ((f(xi)*f(xr)) .le. 0) then
+        xf = xr
+      else
+        xi = xr
+      end if
 
-write(*,*)""
-write(*,*)"Fisrt Column: Index"
-write(*,*)"Second Column: Root"
-write(*,*)"Third Column: F(c)"
-write(*,*)"Fourth Column: Error"
-write(*,*)""
+      er = abs((xr-xrold)/xr)*100
 
-do while (iter <= n)
-  c = (a+b)/2
-  oldc = a
-  if ((f(a))*(f(c)) <= 0 )then
-    b = c
+      write(*,*) iter,xr,f(xr),er
+      write(20,*) iter,xr,f(xr),er
+      iter = iter + 1
+      xrold = xr
+    end do
+    close(20)
   else
-    a = c
-  endif
-
-  er = abs((c-oldc)/c)*100
-  
-  write(*,*) iter, c, f(c),er, "%"
-  write(20,*)iter, c, f(c),er, "%"
-  iter = iter + 1
-end do
-close(20)
-else
-  write(*,*)"=========================================================================="
-  write(*,*)""
-  write(*,*) "> ROOT IS NOT FOUND, BECAUSE f(a)*f(b) > 0. DO NEW ITERATION!!!"
-  write(*,*)""
-  a = a + 1
-  b = b - 1
-  write(*,*)"ITER:",it,"| INITIAL BOUNDARY:",a,"| FINAL BOUNDARY:",b
-  write(*,*)""
-  it = it + 1
-  go to 500
-endif
+    ! Estimate new boundary
+    xi = xi + 1
+    xf = xf - 1
+    go to 50
+  end if
 end program
 
 real function f(x)
-implicit none
-real::x
-f = (x**2) + (2*x) + 1
+  implicit none
+  real::x
+  ! f = (x**10)-1
+  f = (x**2)+(2*x)+1
 end
-
