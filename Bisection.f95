a2665eb9bca1e7abd5410a7b8e8106ea65710073
Aulia Khalqillah <52442929+auliakhalqillah@users.noreply.github.com>
2020-07-21 18:50:45 +0700

    Update Bisection.f95

--- Bisection.f95
+++ Bisection.f95
@@ -8,60 +8,84 @@
 ! email: auliakhalqillah.mail@gmail.com
 ! ==============================================================================
 program bisection_method
-  implicit none
-  real :: xi,xf,xr,er,xrold,f,inf
-  integer :: iter,n,it
-  character(len=100) :: fmt
+implicit none
+real :: xi,xf,xr,er,xrold,f,check,limrange,limit
+integer :: iter, condition
+character(len=100) :: fmt
 
-  write(*,*)""
-  write(*,*)"---------------------------------"
-  write(*,*)"BISECTION METHOD - FINDING ROOT"
-  write(*,*)"---------------------------------"
-  write(*,*) ""
-  write(*,"(a)",advance="no") "Isert Initial Boundary (XI):"
-  read*, xi
-  write(*,"(a)",advance="no") "Insert Final Boundary (XF):"
-  read*, xf
-  write(*,"(a)",advance="no") "Insert Data Length (ex: N=100):"
-  read*, n
+write(*,*)""
+write(*,*)"---------------------------------"
+write(*,*)"BISECTION METHOD - FINDING ROOT"
+write(*,*)"---------------------------------"
+write(*,*) ""
+write(*,"(a)",advance="no") "Insert Initial Boundary (XI):"
+read*, xi
+write(*,"(a)",advance="no") "Insert Final Boundary (XF):"
+read*, xf
 
-  inf = huge(1000.0)
-  fmt = "(a12,a13,a13,a20)"
-  write(*,*)""
-  write(*,fmt)"ITER","XR[ROOT]","F(XR)","ERROR(%)"
-  open(20, file='bisection.txt', status='replace')
+fmt = "(a12,a13,a13,a20,a13,a17,a20,a17,a17,a20)"
+write(*,*)""
 
-  50 if ((f(xi)*f(xf)) .le. 0) then
+limrange = 1e12
+limit = 1e-10
+open(20, file='bisection.txt', status='replace')
+    check = f(xi)*f(xf)
+    do while (check > 0)
+        if (check > limrange) then
+            xi = xi + 1
+            check = f(xi) * f(xf)
+        else
+            xf = xf - 1
+            check = f(xi) * f(xf)
+        end if
+    end do
+
     iter = 1
     xrold = 0.0
-    do while (iter .le. n)
-      xr = (xi+xf)/2
+    xr = (xi+xf)/2
+    er = abs((xr-xrold)/xrold)
+    
+    write(*,fmt)"ITER","Xi","Xf","F(Xi)","F(Xf)","XROLD","XR[ROOT]","F(XR)","ERROR","CONDITION"
+    do while (er > limit .or. isnan(er))      
+        if ((f(xi)*f(xr)) < 0) then
+            xf = xr
+            xrold = xr
+            xr = (xi+xf)/2
+            er = abs((xr-xrold)/xrold)
+            condition = 1
+        elseif ((f(xi)*f(xr)) > 0) then
+            xi = xr
+            xrold = xr
+            xr = (xi+xf)/2
+            er = abs((xr-xrold)/xrold)
+            condition = 2
+        elseif (f(xi)*f(xr) == 0) then
+            xr = xr
 
-      if ((f(xi)*f(xr)) .le. 0) then
-        xf = xr
-      else
-        xi = xr
-      end if
+            if (f(xr) == 0) then
+                xr = xr
+            else
+                xr = xi
+            endif
+            
+            xrold = xr
+            er = abs((xr-xrold)/xrold)
+            condition = 3
+        end if
 
-      er = abs((xr-xrold)/xr)*100
+        write(*,*) iter,xi,xf,f(xi),f(xf),xrold,xr,f(xr),er,condition
+        write(20,*) iter,xi,xf,f(xi),f(xf),xrold,xr,f(xr),er,condition
 
-      write(*,*) iter,xr,f(xr),er
-      write(20,*) iter,xr,f(xr),er
-      iter = iter + 1
-      xrold = xr
+        iter = iter + 1
     end do
-    close(20)
-  else
-    ! Estimate new boundary
-    xi = xi + 1
-    xf = xf - 1
-    go to 50
-  end if
+close(20)
 end program
-
-real function f(x)
-  implicit none
-  real::x
-  ! f = (x**10)-1
-  f = (x**2)+(2*x)+1
-end
+  
+  real function f(x)
+    implicit none
+    real::x
+    ! f = (x**2)-16
+    f = (x**2)+(2*x)+1
+    ! f = (x**3) - (x**2) - (10*x) + 2
+  end
+  
