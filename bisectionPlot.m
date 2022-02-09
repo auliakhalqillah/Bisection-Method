2612dd1669a42cc6629d904d511067347783e588
Aulia Khalqillah <52442929+auliakhalqillah@users.noreply.github.com>
2022-02-09 18:14:07 +0700

    update MATLAB  code for plot bisection

--- bisectionPlot.m
+++ bisectionPlot.m
@@ -1,26 +1,35 @@
 % This matlab program is used to plot results from:
-% 
+%
 % Bisection.f95
-% 
+%
 % By: Aulia Khalqillah,S.Si (2019)
 
 % Numerical Bisection
-clear;clc;
-xi = -3;
-xf = 3;
-n = 100;
-% root from bisection method
-root = -1.00024414;
-deltax = (xf-xi)/n;
-xaxis = [xi:deltax:xf];
-f = (xaxis.^2)+(2*xaxis)+1;
-froot = (root^2)+(2*root)+1;
-plot(xaxis,f)
-hold on;
-plot([root root],[froot froot],'.r','markersize',10)
-xlabel('x')
-ylabel('f(x)')
-title('Finding Root-Bisection Method')
-text(-2.5,12,'f = x^2+2*x+1', 'fontsize', 20)
+data = load('bisection.txt');
+iter = data(:,1);
+root = data(:,7);
+root = root(end);
+fn = data(:,8);
+fn = fn(end);
+error = data(:,9);
+
+% Function
+x = -3:0.1:3;
+f = x.^2 - 2*x + 1;
+
+figure(1)
+plot(x,f,'r','linewidth',2)
+hold on
+plot([root root],[fn fn],'.b','markersize',30)
+legend('Function','Root - Numerical Bisection','location','NorthWest')
+title('FINDING ROOT USING BISECTION METHOD')
+xlabel('X')
+ylabel('Function')
+text(-2.5,12,'f = x^2-2*x+1', 'fontsize', 20)
 grid minor
-print -dpng some_name.png;
+
+figure(2)
+plot(iter,error,'b','linewidth',2.)
+xlabel('Iteration')
+ylabel('Error (%)')
+title('Error of bisection method for each iteration')
